56424f36fe8aac89ba9194a37b2d99f47959f044
Nicolas Trutmann <nicolas.trutmann@gmx.ch>
2023-07-13 17:15:49 +0200

    add window function 1d
            uses sigmoid function to weigh a window in space.

--- src/fbpinns/utils.py
+++ src/fbpinns/utils.py
@@ -1,10 +1,21 @@
-from torch import Tensor
+from typing import Callable
+
+from torch import Tensor, sigmoid
 
 
-def rescale(t: Tensor, lower: Tensor, upper: Tensor):
+def rescale(t: Tensor, lower: Tensor | float, upper: Tensor | float):
     """
-    assumes t to be normalized to [0,1].
+    assumes t to be normalized to [0,1] and scales it into [lower, upper].
     only 1D case tested.
     """
     diff = upper - lower
     return (t * diff) + lower
+
+
+def window_function(
+    lower: Tensor | float, upper: Tensor | float, sigma: float
+) -> Callable[..., Tensor]:
+    def inner(x: Tensor):
+        return sigmoid((x - lower) / sigma) * sigmoid((upper - x) / sigma)
+
+    return inner
